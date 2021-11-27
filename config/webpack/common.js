c86db93048aea8108b82f2c69c503efc4970c4d4
Piotr Praga <piotr.praga@webfleet.com>
2021-11-27 07:10:58 +0100

    webpack config

--- config/webpack/common.js
+++ config/webpack/common.js
@@ -35,10 +35,7 @@ module.exports = {
             },
             {
                 test: /\.(jpe?g|png|gif|svg|ico)$/i,
-                use: [
-                    'file-loader?hash=sha512&digest=hex&name=img/[contenthash].[ext]',
-                    'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false'
-                ]
+                use: ['file-loader?hash=sha512&digest=hex&name=img/[contenthash].[ext]', 'image-webpack-loader']
             }
         ]
     },
